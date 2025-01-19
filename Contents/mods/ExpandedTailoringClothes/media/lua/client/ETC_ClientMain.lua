c476f744366f557257d9d4ebf9b6a0c30ff89cba
Voguh <voguhofc@protonmail.com>
2025-01-19 09:50:08 -0300

    fix: lint

--- Contents/mods/ExpandedTailoringClothes/media/lua/client/ETC_ClientMain.lua
+++ Contents/mods/ExpandedTailoringClothes/media/lua/client/ETC_ClientMain.lua
@@ -1,17 +1,17 @@
 if not ExpandedTailoringClothes then ExpandedTailoringClothes = {} end
 
 function ExpandedTailoringClothes.UpdateProperty(itemType, propertyName, propertyValue)
-  local item = ScriptManager.instance:getItem(itemType);
+    local item = ScriptManager.instance:getItem(itemType);
 
-  if item ~= nil then
-    item:DoParam(propertyName.." = "..propertyValue);
-  end
+    if item ~= nil then
+        item:DoParam(propertyName.." = "..propertyValue);
+    end
 end
 
 function ExpandedTailoringClothes.OnGameBoot()
-  ExpandedTailoringClothes.UpdateProperty("Base.HolsterSimple", "FabricType", "Leather");
-  ExpandedTailoringClothes.UpdateProperty("Base.HolsterDouble", "FabricType", "Leather");
-  ExpandedTailoringClothes.UpdateProperty("Base.Belt2", "FabricType", "Leather");
+    ExpandedTailoringClothes.UpdateProperty("Base.HolsterSimple", "FabricType", "Leather");
+    ExpandedTailoringClothes.UpdateProperty("Base.HolsterDouble", "FabricType", "Leather");
+    ExpandedTailoringClothes.UpdateProperty("Base.Belt2", "FabricType", "Leather");
 end
 
 Events.OnGameBoot.Add(ExpandedTailoringClothes.OnGameBoot)
